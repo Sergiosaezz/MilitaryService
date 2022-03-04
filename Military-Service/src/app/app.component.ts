bfa2ba8d8f9c49963dedaedc80ef5bb69564eea9
Sergio Sáez <sergiosaez94@gmail.com>
2022-03-04 12:34:45 +0100

    feat(app): testing router

--- Military-Service/src/app/app.component.ts
+++ Military-Service/src/app/app.component.ts
@@ -1,18 +1,13 @@
-<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { SoldierDTO } from './shared/domain/DTOs/soldier.dto';
 import { MockService } from './shared/services/mock.service';
-=======
-import { Component } from '@angular/core';
->>>>>>> 06682982389182672a7c842cd1dba3ba70a124cd
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-<<<<<<< HEAD
 export class AppComponent implements OnInit {
   soldiers = new Observable<SoldierDTO[]>();
 
@@ -21,8 +16,4 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.soldiers = this.mockService.loadSoldiers();
   }
-=======
-export class AppComponent {
-  constructor() {}
->>>>>>> 06682982389182672a7c842cd1dba3ba70a124cd
 }
