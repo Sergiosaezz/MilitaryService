858c89541f23c26d7cae4770b6cf5ef9a7c6c244
Sergio Sáez <sergiosaez94@gmail.com>
2022-03-04 13:20:12 +0100

    feat(app): dexie service created

--- Military-Service/src/app/app.component.ts
+++ Military-Service/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { SoldierDTO } from './shared/domain/DTOs/soldier.dto';
+import { DexieService } from './shared/services/dexie.service';
 import { MockService } from './shared/services/mock.service';
 
 @Component({
@@ -11,9 +12,13 @@ import { MockService } from './shared/services/mock.service';
 export class AppComponent implements OnInit {
   soldiers = new Observable<SoldierDTO[]>();
 
-  constructor(private readonly mockService: MockService) {}
+  constructor(
+    private readonly mockService: MockService,
+    private readonly dexieService: DexieService
+  ) {}
 
   ngOnInit(): void {
     this.soldiers = this.mockService.loadSoldiers();
+    this.dexieService.loadData();
   }
 }
