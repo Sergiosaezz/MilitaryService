28f50df7e3315876a2c262f6b0b77c2c5b4cc6d7
Sergio Sáez <sergiosaez94@gmail.com>
2022-03-04 21:19:54 +0100

    build(app): barrack form component added

--- Military-Service/src/app/modules/barracks/barracks.module.ts
+++ Military-Service/src/app/modules/barracks/barracks.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BarracksRouting } from './barracks-routing.module';
 import { BarracksHomeComponent } from './components/barracks-home/barracks-home.component';
+import { BarracksAddComponent } from './components/barracks-add/barracks-add.component';
 
 @NgModule({
-  declarations: [BarracksHomeComponent],
+  declarations: [BarracksHomeComponent, BarracksAddComponent],
   imports: [CommonModule, BarracksRouting],
 })
 export class BarracksModule {}
---
+++ Military-Service/src/app/modules/barracks/components/barracks-add/barracks-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-barracks-add',
+  templateUrl: './barracks-add.component.html',
+  styleUrls: ['./barracks-add.component.css']
+})
+export class BarracksAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
