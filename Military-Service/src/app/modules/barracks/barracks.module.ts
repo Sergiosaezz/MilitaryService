6ff4a291fb949099fe338f2c13bab9bd128c39e1
Sergio Sáez <sergiosaez94@gmail.com>
2022-03-05 18:57:32 +0100

    feat(app): shared module added with material and reactive forms

--- Military-Service/src/app/modules/barracks/barracks.module.ts
+++ Military-Service/src/app/modules/barracks/barracks.module.ts
@@ -4,9 +4,10 @@ import { BarracksRouting } from './barracks-routing.module';
 import { BarracksHomeComponent } from './components/barracks-home/barracks-home.component';
 import { BarracksAddComponent } from './components/barracks-add/barracks-add.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [BarracksHomeComponent, BarracksAddComponent],
-  imports: [CommonModule, BarracksRouting, ReactiveFormsModule],
+  imports: [SharedModule, BarracksRouting],
 })
 export class BarracksModule {}
--- Military-Service/src/app/modules/soldiers/soldiers.module.ts
+++ Military-Service/src/app/modules/soldiers/soldiers.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { SoldiersHomeComponent } from './components/soldiers-home/soldiers-home.component';
 import { SoldiersRouting } from './soldiers-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [SoldiersHomeComponent],
-  imports: [CommonModule, SoldiersRouting],
+  imports: [SharedModule, SoldiersRouting],
 })
 export class SoldiersModule {}
