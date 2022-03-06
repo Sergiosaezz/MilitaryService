d282be173d8640fd977c98bbbd5955c40833429e
miguelopez13 <mlopari216@g.educaand.es>
2022-03-06 13:08:30 +0100

    feat(app): route for edit soldier added

--- Military-Service/src/app/modules/soldiers/soldiers-routing.module.ts
+++ Military-Service/src/app/modules/soldiers/soldiers-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SoldierAddComponent } from './pages/soldier-add/soldier-add.component';
+import { SoldierEditComponent } from './pages/soldier-edit/soldier-edit.component';
 import { SoldiersHomeComponent } from './pages/soldiers-home/soldiers-home.component';
 
 const routes: Routes = [
   { path: '', component: SoldiersHomeComponent },
   { path: 'add', component: SoldierAddComponent },
+  { path: 'edit/:id', component: SoldierEditComponent },
 ];
 
 @NgModule({
